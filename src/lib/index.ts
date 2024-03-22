31806b68f1a9218f385bd6b9c5f8fed11852cec3
nwrenger <nils@wrenger.net>
2024-03-22 10:16:48 +0100

    :wrench: refactor

---
+++ src/lib/index.ts
@@ -0,0 +1,24 @@
+export let websites = [
+	{
+		name: 'protfolio',
+		title: 'Personal Portfolio',
+		href: 'https://portfolio.nwrenger.dev',
+		description: 'Ideally for visitors who want to know more about me and my projects!',
+		disabled: false
+	},
+	{
+		name: 'quickmaths',
+		title: 'Math Practicing Website',
+		href: 'https://quickmaths.nwrenger.dev',
+		description:
+			'Want to improve your skills at maths, especially at calculations, this is the best place for that!',
+		disabled: false
+	},
+	{
+		name: 'shitboard',
+		title: 'Shitboard (A Soundboard)',
+		href: 'https://shitboard.nwrenger.dev',
+		description: 'A Chaos-Fueled Soundboard App where creativity and permanence collide!',
+		disabled: true
+	}
+];
--- src/lib/screenshots.ts
+++ src/lib/screenshots.ts
@@ -1,23 +1,6 @@
 // generating in build && dev process
 import puppeteer from 'puppeteer';
-
-let websites = [
-	{
-		name: 'protfolio',
-		href: 'https://portfolio.nwrenger.dev',
-		disabled: false
-	},
-	{
-		name: 'quickmaths',
-		href: 'https://quickmaths.nwrenger.dev',
-		disabled: false
-	},
-	{
-		name: 'shitboard',
-		href: 'https://shitboard.nwrenger.dev',
-		disabled: true
-	}
-];
+import { websites } from '.';
 
 async function captureScreenshot(url: string, outputPath: string): Promise<void> {
 	const browser = await puppeteer.launch();
